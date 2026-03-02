4386c8abc4c45eae33659fd5ca9905bba3a3e89c
RCD-2023 <codreanu_danielromeo@yahoo.com>
2026-03-02 18:58:42 +0200

    final day commit

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-# Create your views here.
+# Create your views methods here.
+
+def register(request):
+    if request.method =='POST':
+        # Register user
+        pass
+    else:
+        return render(request, 'accounts/register.html')
+
+def login(request):
+    if request.method =='POST':
+        # Login user
+        pass
+    else:
+        return render(request, 'accounts/login.html')
+
+def logout(request):
+    return redirect('index')
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
